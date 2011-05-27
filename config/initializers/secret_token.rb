22d15c664686578b91ecaca56e599150e4fd7927
John Renner <johnmrenner@gmail.com>
2011-05-27 16:59:17 -0400

    Proper Gemfile

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-DemoApp::Application.config.secret_token = '89eee819bd57312a87aa9cdf329863d354a1383c96517cd3590ee47805aa6c81481b6e51d2d8910390d5601178986c09505f000e5394c7f82d7b1682c13f2d19'
+DemoApp::Application.config.secret_token = 'ea24f4bc9556abffb90c64a6c6d9344922cb39b4351aa4a966f7187bc8fce25a0ad11734a721eab741ee098d1c3ec4197ef688c055bef70c284306ae5cfd94b4'
